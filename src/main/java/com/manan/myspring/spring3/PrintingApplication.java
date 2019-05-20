6d237e5942f3f790763196a5f1f2f0704abe1a40
Manan Gakhar <manang168@gmail.com>
2019-05-20 05:51:18 +0530

    trying context from both classpath and file

--- src/main/java/com/manan/myspring/spring3/PrintingApplication.java
+++ src/main/java/com/manan/myspring/spring3/PrintingApplication.java
@@ -2,14 +2,22 @@ package com.manan.myspring.spring3;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class PrintingApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
-        final IPrinter iPrinter = (IPrinter) context.getBean("printer");
-        iPrinter.print();
-        System.out.println();
+        // bean file to be put in resources for classpath
+        ApplicationContext contextFromResource = new ClassPathXmlApplicationContext("SpringBeansResource.xml");
+        // bean file can be put anywhere if file system context
+        ApplicationContext contextFromFile = new FileSystemXmlApplicationContext
+                ("src/main/java/com/manan/myspring/spring3/SpringBeansFile.xml");
+        // prints json
+        final IPrinter iPrinterFromResource = (IPrinter) contextFromResource.getBean("printer");
+        iPrinterFromResource.print();
+        // prints csv
+        final IPrinter iPrinterFromFile = (IPrinter) contextFromFile.getBean("printer");
+        iPrinterFromFile.print();
+        
     }
 }
