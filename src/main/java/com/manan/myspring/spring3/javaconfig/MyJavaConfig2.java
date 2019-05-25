45040f604ae8e9a3d32d1478234778f982abaa87
Manan Gakhar <manang168@gmail.com>
2019-05-26 04:11:29 +0530

    @Import : importing beans from another file in java

--- src/main/java/com/manan/myspring/spring3/javaconfig/MyJavaConfig2.java
+++ src/main/java/com/manan/myspring/spring3/javaconfig/MyJavaConfig2.java
@@ -2,14 +2,12 @@ package com.manan.myspring.spring3.javaconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Import(MyJavaConfig2.class)
 @Configuration
-public class MyJavaConfig {
+public class MyJavaConfig2 {
 
-    @Bean // here the bean name is the method name
-    public IHelloWorld helloWorld() {
+    @Bean(name = "helloBean") // can give multiple names too. giving name does not allow to get bean by method name
+    public IHelloWorld helloWorldBean() {
         return new HelloWorldImpl();
     }
 }
